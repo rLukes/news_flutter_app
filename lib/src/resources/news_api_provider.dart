3af5aeea228b048fe18cddfbdf9aabc679116866
rlukes <lukesruben@gmail.com>
2018-12-14 23:09:26 +0100

    added api and db provider

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,19 +1,24 @@
 import 'dart:convert';
 import 'package:http/http.dart' show Client;
 import '../models/item_model.dart';
+import 'dart:async';
+import 'repository.dart';
 
 final _root = 'https://hacker-news.firebaseio.com/v0';
 
-class NewsApiProvider{
+class NewsApiProvider implements Source{
   Client client = Client();
 
-  fetchTopIds() async{
+  /*
+  since we use async and await, the return type must be future (wrap this return type with a future)
+   */
+  Future<List<int>> fetchTopIds() async{
     final response = await client.get('$_root/topstories.json?print=pretty');
     final ids = json.decode(response.body);
-    return ids;
+    return ids.cast<int>();
   }
 
-  fetchItem(int id) async{
+  Future<ItemModel> fetchItem(int id) async{
     final response = await client.get('$_root/item/$id.json?');
     final parsedJson = json.decode(response.body);
     return ItemModel.fromJson(parsedJson);
---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'news_api_provider.dart';
+import 'news_db_provider.dart';
+import '../models/item_model.dart';
+
+abstract class Source {
+ Future<List<int>> fetchTopIds();
+ Future<ItemModel> fetchItem(int id);
+}
+
+abstract class Cache{
+  Future<int> addItem(ItemModel item);
+}
+
+class Repository {
+  List<Source> sources = <Source>[
+    NewsApiProvider(),
+    newsDbProvicder,
+  ];
+
+  List<Cache> caches = <Cache>[
+    NewDbProvider(),
+    newsDbProvicder,
+  ];
+
+  //iterate over sources when dbprovider get fetchTopIds impemented
+  Future<List<int>> fetchTopIds(){
+    return sources[1].fetchTopIds();
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    ItemModel item;
+    Source source;
+    for(source in sources){
+      item = await source.fetchItem(id);
+      if(item != null)
+        break;
+    }
+    for(var c in caches){
+      c.addItem(item);
+    }
+    return item;
+  }
+}
